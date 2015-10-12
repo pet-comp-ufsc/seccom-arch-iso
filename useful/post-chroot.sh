cdab3fb0bccbc02f48e42010a0116fcb32e4f85d
Ion Neto <ionneto@gmail.com>
2015-10-12 17:13:52 -0300

    Add some packages and fixed permissions

--- useful/post-chroot.sh
+++ useful/post-chroot.sh
@@ -1,5 +1,7 @@
 systemctl enable slim.service
 systemctl enable mount-home.service
+systemctl enable dhcpcd
 rm -rf /home/seccom/{*,.*} > /dev/null 2>&1
 chmod -R u+x /seccom
 chown -R seccom:users /seccom
+chsh -s /bin/zsh seccom
--- useful/seccom/mount_home.sh
+++ useful/seccom/mount_home.sh
@@ -2,6 +2,7 @@
 dir=$(curl http://pet.inf.ufsc.br/get_home_dir)
 sshfs seccom@pet.inf.ufsc.br:${dir} ${HOME} \
     -o IdentityFile=/seccom/seccom_key \
+    -o idmap=file,uidfile=/seccom/uidmapping,gidfile=/seccom/gidmapping\
     -o UserKnownHostsFile=/dev/null \
     -o StrictHostKeyChecking=no \
     -F /dev/null
